7c24b724803bcb9863c3e731cc2e636fa9b8da87
James Gosbell <3621147+jamesg1@users.noreply.github.com>
2018-06-04 21:51:11 +1000

    Added cornerstone tools, added range slider hooked up to state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,47 @@ import Header from './components/header';
 import ImageSeriesViewer from './components/image_series_viewer';
 import './App.css';
 
+export const PUBLIC_URL = 'http://localhost:3000';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      imageResults: [],
+      stack: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/image_series.json', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      this.setState({
+        imageResults: response.Results,
+        loading: false,
+        stack: {
+          imageIds: response.Results.map(image => `${PUBLIC_URL}/${image.Dicom}`),
+          currentImageIdIndex: 0
+        }
+      });
+    });
+  }
+
   render() {
-    return (
+    const { loading, stack } = this.state;
+    return !loading ? (
       <div className="app">
         <Header />
-        <ImageSeriesViewer />
+        <ImageSeriesViewer stack={stack} />
       </div>
-    );
+    ) : null;
   }
 }
 
--- src/components/image_series_viewer/image_series_viewer.js
+++ src/components/image_series_viewer/image_series_viewer.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import Hammer from "hammerjs";
 import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneTools from 'cornerstone-tools';
 import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
 import './image_series_viewer.css';
 
+// Initialise References for CornerstoneTools Plugin
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.Hammer = Hammer;
+
+// Initialise References for Cornerstone Web Image Loader
 cornerstoneWebImageLoader.external.cornerstone = cornerstone;
 
 class ImageSeriesViewer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.range = React.createRef();
+
+    this.state = {
+      currentImageIdIndex: props.stack.currentImageIdIndex,
+      stack: props.stack
+    };
+  }
+
+  handleRangeChange = (event) => {
+    this.setState({ currentImageIdIndex: event.target.value });
+  }
+
   render() {
+    const { currentImageIdIndex, stack } = this.state;
     return (
       <div className="container">
-        <p>Viewer</p>
+        <div className="controls">
+          <input
+            type="range"
+            min={0}
+            max={stack.imageIds.length - 1}
+            value={currentImageIdIndex}
+            onChange={this.handleRangeChange}
+            ref={this.range} />
+        </div>
+        {currentImageIdIndex}
+        <div
+          ref={input => {
+            this.element = input;
+          }}
+          className="viewer">
+          <canvas className="cornerstone-canvas" />
+        </div>
       </div>
     );
   }
+
 }
 
 export default ImageSeriesViewer;
