e38794d5b2b3f231bafbc3c78c16b6c361657777
James Gosbell <3621147+jamesg1@users.noreply.github.com>
2018-06-06 00:58:14 +1000

    Added overlay for shaded section on image_viewer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header';
 import ImageSeriesViewer from './components/image_series_viewer';
 import './App.css';
 
-export const PUBLIC_URL = 'http://localhost:3000';
+export const BASEURI = 'http://localhost:3000';
 
 class App extends Component {
 
@@ -18,30 +18,33 @@ class App extends Component {
 
   componentDidMount() {
     fetch('/api/image_series.json', {
-      headers : { 
+      headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
-       }
+      }
     })
-    .then((response) => response.json())
-    .then((response) => {
-      this.setState({
-        imageResults: response.Results,
-        loading: false,
-        stack: {
-          imageIds: response.Results.map(image => `${PUBLIC_URL}/${image.Dicom}`),
-          currentImageIdIndex: 0
-        }
-      });
+      .then((response) => response.json())
+      .then((response) => this.setImageResults(response));
+  }
+
+  setImageResults = response => {
+    const imageResults = response.Results.sort((a, b) => a.Length - b.Length);
+    this.setState({
+      imageResults,
+      stack: {
+        imageIds: imageResults.map(image => `${BASEURI}/${image.Dicom}`),
+        currentImageIdIndex: 0
+      },
+      loading: false
     });
   }
 
   render() {
-    const { loading, stack } = this.state;
+    const { loading, imageResults, stack } = this.state;
     return !loading ? (
       <div className="app">
         <Header />
-        <ImageSeriesViewer stack={stack} />
+        <ImageSeriesViewer imageResults={imageResults} stack={stack} />
       </div>
     ) : null;
   }
--- src/components/image_series_viewer/image_series_viewer.js
+++ src/components/image_series_viewer/image_series_viewer.js
@@ -5,49 +5,136 @@ import * as cornerstoneTools from 'cornerstone-tools';
 import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
 import './image_series_viewer.css';
 
-// Initialise References for CornerstoneTools Plugin
+// Specify External Dependencies for CornerstoneTools Plugin
 cornerstoneTools.external.cornerstone = cornerstone;
 cornerstoneTools.external.Hammer = Hammer;
-
-// Initialise References for Cornerstone Web Image Loader
+// Specify External Dependencies for Cornerstone Web Image Loader
 cornerstoneWebImageLoader.external.cornerstone = cornerstone;
 
 class ImageSeriesViewer extends Component {
 
   constructor(props) {
     super(props);
-    this.range = React.createRef();
-
     this.state = {
-      currentImageIdIndex: props.stack.currentImageIdIndex,
-      stack: props.stack
+      overlay: 'transparent',
+      stack: props.stack,
+      currentImage: props.stack.imageIds[0]
     };
   }
 
-  handleRangeChange = (event) => {
-    this.setState({ currentImageIdIndex: event.target.value });
+  componentDidMount() {
+    const viewer = this.viewer;
+    const { currentImage } = this.state;
+
+    cornerstone.enable(viewer);
+    cornerstoneTools.mouseInput.enable(element);
+    cornerstoneTools.mouseWheelInput.enable(viewer);
+    cornerstone.loadImage(currentImage).then(image => {
+      cornerstone.displayImage(viewer, image);
+      this.setupViewerStack(viewer);
+      this.initialiseEventListeners();
+    });
+    this.setupOverlay();
+  }
+
+  setupViewerStack = (viewer) => {
+    const stack = this.state.stack;
+    cornerstoneTools.addStackStateManager(viewer, ["stack"]);
+    cornerstoneTools.addToolState(viewer, "stack", stack);
+    cornerstoneTools.stackScroll.activate(viewer, 1);
+    cornerstoneTools.stackScrollWheel.activate(viewer);
+    cornerstoneTools.scrollIndicator.enable(viewer);
+  }
+
+  componentDidUmount() {
+    window.removeEventListener("resize", this.onWindowResize);
+    this.viewer.removeEventListener("cornerstonenewimage", this.onNewImage);
+    cornerstone.disable(this.viewer);
+  }
+
+  initialiseEventListeners = () => {
+    // Listens for window resize before resizing viewer canvas 
+    window.addEventListener("resize", this.onWindowResize);
+    // Sets current currentImageIdIndex State
+    this.viewer.addEventListener('cornerstonenewimage', this.onNewImage);
+  }
+
+  onNewImage = () => {
+    this.setupOverlay();
+    const enabledElement = cornerstone.getEnabledElement(this.viewer);
+    this.setState({
+      currentImage: enabledElement.image.imageId
+    });
+  }
+
+  onWindowResize = () => {
+    cornerstone.resize(this.viewer);
+  }
+
+  getImageCountText = () => {
+    const { currentImageIdIndex, imageIds } = this.state.stack;
+    // This starts the count from 1 rather than 0. eg 1-20 rather than 1-19
+    return `${currentImageIdIndex + 1} of ${imageIds.length}`;
+  }
+
+  setupOverlay = () => {
+    const url = `images/overlay_${this.state.stack.currentImageIdIndex}.png`;
+    this.covertOverlayToCanvas(url);
+  }
+
+  covertOverlayToCanvas = (url) => {
+    let canvas = this.canvas;
+    let ctx = canvas.getContext("2d");
+
+    return new Promise(resolve => {
+      var image = new Image();
+      image.src = url;
+      image.onload = () => {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.globalAlpha = 0.7;
+        ctx.drawImage(image, 0, 0);
+
+        let imageData = ctx.getImageData(0, 0, image.width, image.height);
+        let data = imageData.data;
+
+        for (let i = 0; i < data.length; i += 4) {
+          let rgbColor = data[i] + data[i + 1] + data[i + 2];
+          // Turning Black to transparent pixels
+          if (rgbColor < 10) {
+            data[i + 3] = 0; // Chang rgb alpha to 0
+          }
+          // Check if white - change to magenta
+          if (rgbColor === 765) {
+            imageData.data[i] = 191;
+            imageData.data[i + 1] = 0;
+            imageData.data[i + 2] = 255;
+          }
+          // Check if grey - change to yellow
+          if (rgbColor === 381) {
+            imageData.data[i] = 230;
+            imageData.data[i + 1] = 230;
+            imageData.data[i + 2] = 0;
+          }
+        }
+        resolve(ctx.putImageData(imageData, 0, 0));
+      };
+    });
   }
 
   render() {
-    const { currentImageIdIndex, stack } = this.state;
     return (
       <div className="container">
-        <div className="controls">
-          <input
-            type="range"
-            min={0}
-            max={stack.imageIds.length - 1}
-            value={currentImageIdIndex}
-            onChange={this.handleRangeChange}
-            ref={this.range} />
-        </div>
-        {currentImageIdIndex}
         <div
           ref={input => {
-            this.element = input;
+            this.viewer = input;
           }}
           className="viewer">
           <canvas className="cornerstone-canvas" />
+          <canvas className="viewer__overlay" ref={canvas => { this.canvas = canvas }} />
+          <div className="viewer__label">
+            Image: {this.getImageCountText()}
+          </div>
         </div>
       </div>
     );
